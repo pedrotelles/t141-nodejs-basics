0e01b153ee3813891fe846042f8a1362343614fb
lorddorn <pedrotelles@gec.inatel.br>
2017-07-01 12:19:39 -0300

    added npm example

--- 03-npm/index.js
+++ 03-npm/index.js
@@ -1,4 +1,8 @@
 
 const string = require('useful-string')
+const _ = require('lodash')
+_.times(4,() =>{
 
-console.log(string.toCamelCase('Pedro Henrique Telles'))
+    //console.log(string.toCamelCase('Pedro Henrique Telles'))
+    console.log("id: "+string.guid())
+})
